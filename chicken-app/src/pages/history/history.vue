50f29aa95dd14e0412e60a0ea72451606f92b193
Accelerator-xm <2637746605@qq.com>
2025-07-17 16:55:26 +0800

    feat: 优化个人中心界面 - 美化个人中心 - 修复历史订单图片不显示

--- chicken-app/src/pages/history/history.vue
+++ chicken-app/src/pages/history/history.vue
@@ -23,7 +23,7 @@
           <view class="history_item_order_id">订单号：{{ item.number }}</view>
           <scroll-view class="scroll_container" scroll-x>
             <view v-for="(dish, index) in item.orderDetailList" :key="index" class="image_box">
-              <image :src="dish.pic" />
+              <image :src="getImageUrl(dish.pic ?? '')" />
             </view>
           </scroll-view>
           <view class="history_item_order_time">{{ item.orderTime }}</view>
@@ -53,6 +53,7 @@ import {onLoad, onReachBottom} from '@dcloudio/uni-app'
 import {getOrderPageAPI, reOrderAPI} from '@/api/order'
 import {cleanCartAPI} from '@/api/cart'
 import type {OrderPageDTO, OrderVO} from '@/types/order'
+import { getImageUrl } from '@/utils/imageUrl'
 
 const childComp: any = ref(null)
 
